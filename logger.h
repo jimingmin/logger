bef91d90927abae1478bff80e6d6c9ad2c86317e
jimingmin <jimingmin@gmail.com>
2014-05-29 00:12:49 +0800

    宏定义略改

--- logger.h
+++ logger.h
@@ -42,13 +42,13 @@ protected:
 	static bool		m_bStart;
 };
 
-#define WRITE_DEBUG_LOG(LOGGERNAME, FORMAT, ...)		CLogger::Debug(LOGGERNAME, __FILE__, __LINE__, FORMAT, __VA_ARGS__)
+#define WRITE_DEBUG_LOG(LOGGERNAME, FORMAT, ...)		CLogger::Debug(LOGGERNAME, __FILE__, __LINE__, FORMAT, ##__VA_ARGS__)
 
-#define WRITE_INFO_LOG(LOGGERNAME, FORMAT, ...)			CLogger::Info(LOGGERNAME, __FILE__, __LINE__, FORMAT, __VA_ARGS__)
+#define WRITE_INFO_LOG(LOGGERNAME, FORMAT, ...)			CLogger::Info(LOGGERNAME, __FILE__, __LINE__, FORMAT, ##__VA_ARGS__)
 
-#define WRITE_WARN_LOG(LOGGERNAME, FORMAT, ...)			CLogger::Warn(LOGGERNAME, __FILE__, __LINE__, FORMAT, __VA_ARGS__)
+#define WRITE_WARN_LOG(LOGGERNAME, FORMAT, ...)			CLogger::Warn(LOGGERNAME, __FILE__, __LINE__, FORMAT, ##__VA_ARGS__)
 
-#define WRITE_ERROR_LOG(LOGGERNAME, FORMAT, ...)		CLogger::Error(LOGGERNAME, __FILE__, __LINE__, FORMAT, __VA_ARGS__)
+#define WRITE_ERROR_LOG(LOGGERNAME, FORMAT, ...)		CLogger::Error(LOGGERNAME, __FILE__, __LINE__, FORMAT, ##__VA_ARGS__)
 
 LOGGER_NAMESPACE_END
 
